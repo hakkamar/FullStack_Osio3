64fa2e426fe2b001fa6c527f28bc683ecbef29ef
hakkamar <hakkis68@gmail.com>
2018-08-07 10:10:06 +0300

    3.2 puhelinluettelon backend osa 2 ja paikkaus edelliseen

--- index.js
+++ index.js
@@ -26,13 +26,26 @@ let persons = [
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
-  })
+})
   
-  app.get('/persons', (req, res) => {
+app.get('/info', (req, res) => {
+    //console.log(' tää on /info')
+    //console.log('puhelinluettelossa ', persons.length, ' henkilön tiedot')
+    //console.log(Date())
+
+    const montako = 'puhelinluettelossa ' + persons.length + ' henkilön tiedot'
+    const nyt = Date()
+    const alku = '<p>'
+    const loppu = '</p>'
+    const tulostettava = alku + montako + loppu + alku + nyt + loppu
+    res.send(tulostettava)
+})
+
+app.get('/api/persons', (req, res) => {
     res.json(persons)
-  })
+})
   
-  const PORT = 3001
-  app.listen(PORT, () => {
+const PORT = 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
